d5552f7869dfaa0adccc0bd213b6911612784191
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-01-12 06:54:13 -0800

    Add Sample Code

---
+++ Samples/FirstPersonInteractions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonInteractions : MonoBehaviour
+{
+    public Camera playerCamera;
+    public MineQuest.World world;
+    public GameObject markerPrefab;
+
+    private GameObject marker;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            world.Interaction.HitBlock(playerCamera.ScreenPointToRay(Input.mousePosition));
+        else if (Input.GetMouseButtonDown(1))
+            world.Interaction.InsertBlock(playerCamera.ScreenPointToRay(Input.mousePosition), MineQuest.Block.Type.Redstone);
+
+    }
+}
---
+++ Samples/WorldStart.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class WorldStart : MonoBehaviour
+{
+    public string worldFile;
+    public MineQuest.World world;
+    public GameObject player;
+
+    public MineQuest.WorldBuilder WorldBuilder { get; private set; }
+    private Vector3Int worldMin = new Vector3Int(-4, 0, -4);
+    private Vector3Int worldMax = new Vector3Int(4, 10, 4);
+
+    private float buildStartTime;
+
+    void Start()
+    {
+        player.GetComponent<CharacterController>().enabled = false;
+
+        if (File.Exists(worldFile))
+        {
+            WorldReady();
+        }
+        else
+        {
+            WorldBuilder = new MineQuest.WorldBuilder();
+            buildStartTime = Time.realtimeSinceStartup;
+            WorldBuilder.Build(worldFile, worldMin, worldMax);
+        }
+    }
+
+    void Update()
+    {
+        if (WorldBuilder != null)
+        {
+            if (WorldBuilder.CreatedChunks == WorldBuilder.TotalChunks)
+            {
+                var buildEndTime = Time.realtimeSinceStartup;
+                Debug.Log(string.Format("Built {0} world chunks in {1} seconds", WorldBuilder.TotalChunks, buildEndTime - buildStartTime));
+                WorldReady();
+                WorldBuilder = null;
+            }
+        }
+    }
+
+    private void WorldReady()
+    {
+        world.Open(worldFile);
+
+        // determine the correct initial position for the player
+        var playerPos = player.transform.position;
+        playerPos.y = world.GetWorldHeight(playerPos.x, playerPos.z) + 1;
+        player.transform.position = playerPos;
+
+        world.LoadInitial();
+        player.GetComponent<CharacterController>().enabled = true;
+
+        GameObject.Destroy(this.gameObject);
+    }
+}
